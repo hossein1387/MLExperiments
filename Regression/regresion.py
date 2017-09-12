eda90e125b40f7917d3e2bd146be7c054713eedd
Hossein <m.h.askari.hemmat@gmail.com>
2017-09-12 00:47:23 -0400

    adding knobes to control

--- Regression/regresion.py
+++ Regression/regresion.py
@@ -1,28 +1,32 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Generating data
+# Generating target data
 m = np.arange(-5,5, 0.1)
 n = -3*m + 2 + 2*np.random.rand(len(m))
 
 # Creating our model
 a = 1
 b = 0
-x = np.arange(-5,5, 0.1)
+x = np.arange(-5, 5, 0.1)
 y = a*x + b
+
+# Update knobes
 thr = 0.001
 prev_cost = 10000
 cost = 0
 iter=0
+MAX_COUNT = 10000
+learning_rate = 0.0001
 
-while (np.abs(cost-prev_cost) >= 0.001):
+while (np.abs(cost-prev_cost) >= thr) and (iter < MAX_COUNT):
     y = a*x + b
     prev_cost = cost
     cost = np.sum(np.abs(y-n))
     da = np.sum(np.sign(y-n)*x)
     db = np.sum(np.sign(y-n))
-    a = a - 0.001*da
-    b = b - 0.001*db
+    a = a - learning_rate*da
+    b = b - learning_rate*db
     iter += 1
     print "iter=", iter, " cost=", cost
 
