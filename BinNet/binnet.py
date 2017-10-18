d40e0905b3b6c70209677de4e53efe8a019f4060
Hossein <m.h.askari.hemmat@gmail.com>
2017-10-17 22:18:03 -0400

    with overriding gradients

--- BinNet/binnet.py
+++ BinNet/binnet.py
@@ -7,14 +7,37 @@ from theano.tensor.signal import pool
 import lasagne
 from theano.sandbox.rng_mrg import MRG_RandomStreams as RandomStreams
 import pdb
+from theano import gof
+
+
+class BackProbableBinarize(gof.Op):
+    __props__ = ()
+
+    def make_node(self, x):
+        return gof.Apply(self, [x], [x.type()])
+
+    def perform(self, node, inp, out):
+        x, = inp
+        z, = out
+        z[0] = np.sign(x)
+
+    def grad(self, args, g_outs):
+        i = args[0]
+        gt1 = T.switch(T.gt(i, 1), 1, 0)
+        lsm1 = T.switch(T.lt(i, -1), 1, 0)
+        gi = 1 - gt1 - lsm1
+        return [g * gi for g in g_outs]
+
+def back_probable_binarize(x):
+    return BackProbableBinarize()(x)
 
 ##################################################################################
 # Globals
 ##################################################################################
 H = 1
-num_epochs = 10
+num_epochs = 250
 epsilon = 1e-6
-lr_start = .001
+lr_start = .1
 lr_fin = 0.000003
 lr_decay = (lr_fin/lr_start)**(1./num_epochs)
 batch_size = 100
@@ -67,28 +90,28 @@ lr    = T.scalar(name='lr')
 ##################################################################################
 # Computational Graph
 ##################################################################################
-w1_b    = binarize_theano(w_1)
+w1_b    = back_probable_binarize(w_1)
 pa_1    = T.nnet.conv2d(x, w1_b)
 mu_1    = T.mean(pa_1, axis=0, keepdims=True)
 std_1   = T.sqrt(T.var(pa_1, axis=0, keepdims=True) + 1e-6) #T.std didn't work by it self on this layer, std was too small
 bn_1    = ((pa_1 - mu_1)/(std_1+epsilon))*gama_1.dimshuffle('x', 0, 'x', 'x') + b_1.dimshuffle('x', 0, 'x', 'x')
-a_1     = binarize_theano(pool.pool_2d(T.tanh(bn_1), (2, 2), ignore_border=True))
+a_1     = back_probable_binarize(pool.pool_2d(T.tanh(bn_1), (2, 2), ignore_border=True))
 
-w2_b    = binarize_theano(w_2)
+w2_b    = back_probable_binarize(w_2)
 pa_2    = T.nnet.conv2d(a_1, w2_b)
 mu_2    = T.mean(pa_2, axis=0, keepdims=True)
 std_2   = T.std(pa_2, axis=0, keepdims=True)
 bn_2    = ((pa_2 - mu_2)/(std_2+epsilon))*gama_2.dimshuffle('x', 0, 'x', 'x') + b_2.dimshuffle('x', 0, 'x', 'x')
-a_2     = binarize_theano(pool.pool_2d(T.tanh(bn_2), (2, 2), ignore_border=True))
+a_2     = back_probable_binarize(pool.pool_2d(T.tanh(bn_2), (2, 2), ignore_border=True))
 
-w3_b    = binarize_theano(w_3)
+w3_b    = back_probable_binarize(w_3)
 pa_3    = T.dot(a_2.flatten(2), w3_b)
 mu_3    = T.mean(pa_3, axis=0, keepdims=True)
 std_3   = T.std(pa_3, axis=0, keepdims=True)
 bn_3    = ((pa_3 - mu_3)/(std_3+epsilon))*gama_3.dimshuffle('x', 0) + b_3.dimshuffle('x', 0)
-a_3     = binarize_theano(T.tanh(bn_3))
+a_3     = back_probable_binarize(T.tanh(bn_3))
 
-w4_b    = binarize_theano(w_4)
+w4_b    = back_probable_binarize(w_4)
 pa_4    = T.dot(a_3, w4_b)
 mu_4    = T.mean(pa_4, axis=0, keepdims=True)
 std_4   = T.std(pa_4, axis=0, keepdims=True)
@@ -101,10 +124,10 @@ y_hat   = a_4
 ##################################################################################
 cost = T.mean(T.nnet.categorical_crossentropy(y_hat, y))
 cost.name = 'CE'
-params_b  = [ gama_1, w1_b, b_1, gama_2, w2_b , b_2, gama_3, w3_b, b_3, gama_4, w4_b, b_4] # Binary weights
+# params_b  = [ gama_1, w1_b, b_1, gama_2, w2_b , b_2, gama_3, w3_b, b_3, gama_4, w4_b, b_4] # Binary weights
 params    = [ gama_1, w_1,  b_1, gama_2, w_2 ,  b_2, gama_3, w_3,  b_3, gama_4, w_4,  b_4] # Float weights
 # Computing gradients with regards to binary weights
-dparams   = T.grad(cost, params_b)
+dparams   = T.grad(cost, params)
 updates   = []
 
 for p, dp in zip(params, dparams):
@@ -129,11 +152,11 @@ learning_rate = lr_start
 for e in range(num_epochs):
     batch_errs = []
     print "learning_rate:" + str(learning_rate)
-    for i in range(batch_size/10):
+    for i in range(batch_size):
         batch_img = data_img[i] / 255.0
         batch_tar = data_tar[i]
         pa, cost  = f_train(batch_img, learning_rate, batch_tar)
-    for i in range(batch_size/10):
+    for i in range(batch_size):
         batch_img = data_img[i] / 255.0
         batch_tar = data_tar[i]
         pred = np.argmax(f_eval(batch_img), axis=1)
@@ -155,5 +178,7 @@ for e in range(num_epochs):
 #        print dp.eval()
     learning_rate *= lr_decay
     print 'error: ' + str((1 - np.mean(batch_errs)) * 100) + "\n"
-#    dparams_ = f_get_grad(batch_img, batch_tar)
+    dparams_ = f_get_grad(batch_img, batch_tar)
+    # for dp, dp_ in zip(dparams, dparams_):
+    #     print str(dp) + ' : ' + str(np.sum(dp_ ** 2))
 #    import pdb; pdb.set_trace()
